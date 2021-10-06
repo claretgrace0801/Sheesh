ca63ab0cb5dd4e6b4d35200f7c86356e20051980
shreyas <shpradhan12@gmail.com>
2021-10-06 20:38:29 +0530

    shehhehehehehhssssss

--- Makefile
+++ Makefile
@@ -1,13 +1,13 @@
 all: shell clear run
 
 shell: compilation
-	gcc -g -o shell main.o helpers.o prompt.o parser.o jobs.o test.o cd.o echo.o pinfo.o ls.o repeat.o history.o
+	gcc -g -o shell main.o helpers.o prompt.o parser.o execution.o test.o cd.o echo.o pinfo.o ls.o repeat.o history.o
  
 compilation:
-	gcc -c -Wall -Wextra main.c helpers.c test.c prompt.c parser.c jobs.c cd.c echo.c pinfo.c ls.c repeat.c history.c
+	gcc -c -Wall -Wextra main.c helpers.c test.c prompt.c parser.c execution.c cd.c echo.c pinfo.c ls.c repeat.c history.c
 
 clear:
-	rm main.o helpers.o test.o prompt.o parser.o jobs.o cd.o echo.o pinfo.o ls.o repeat.o history.o
+	rm main.o helpers.o test.o prompt.o parser.o execution.o cd.o echo.o pinfo.o ls.o repeat.o history.o
 
 run:
 	./shell
--- test/testing.c
+++ test/testing.c
@@ -12,90 +12,23 @@
 #include <grp.h>
 #include <time.h>
 
-void append_char(char **s, char c)
-{
-  int n = strlen(*s);
-  *s = realloc(*s, n + 2);
-  (*s)[n] = c;
-  (*s)[n + 1] = '\0';
-}
-
-struct ints
-{
-  int *arr;
-  int sz;
-};
-
-void add_to_int_arr(struct ints *arr, int n)
-{
-  arr->arr = realloc(arr->arr, (arr->sz + 1) * sizeof(int));
-  arr->arr[arr->sz] = n;
-  arr->sz = arr->sz + 1;
-}
-
 int main()
 {
-  char *s = malloc(6);
-  strcpy(s, "hello");
+  printf("stared\n");
 
-  struct ints arr;
-  arr.arr = malloc(sizeof(int));
-  arr.sz = 0;
+  pid_t pid = fork();
 
-  add_to_int_arr(&arr, 1);
-  add_to_int_arr(&arr, 2);
-  add_to_int_arr(&arr, 3);
-  add_to_int_arr(&arr, 4);
-  add_to_int_arr(&arr, 1);
-  add_to_int_arr(&arr, 5);
-  add_to_int_arr(&arr, 1);
-  add_to_int_arr(&arr, 1);
-
-  for (int i = 0; i < arr.sz; i++)
+  if (pid == 0)
   {
-    printf("%d\n", arr.arr[i]);
+    char *args[2] = {"gedit", NULL};
+    execvp("gedit", args);
   }
-
-  // // printf("%s", s);
-
-  // int p[2];
-  // if (pipe(p) < 0)
-  // {
-  //   perror("Pipe Fucked");
-  // }
-
-  // // echo
-
-  // int echo_pid = fork();
-
-  // close(p[1]);
-
-  // if (echo_pid == 0)
-  // {
-  //   // dup2(p[1], 1);
-
-  //   char *echo_args[3] = {"echo", "hello there", NULL};
-  //   execvp("echo", echo_args);
-  // }
-  // else
-  // {
-  //   waitpid(echo_pid, NULL, 0);
-  // }
-
-  // // cat
-
-  // int cat_pid = fork();
-
-  // if (cat_pid == 0)
-  // {
-  //   dup2(p[0], 0);
-
-  //   char *cat_args[2] = {"ls", NULL};
-  //   execvp("ls", cat_args);
-  // }
-  // else
-  // {
-  //   waitpid(cat_pid, NULL, 0);
-  //   close(p[0]);
-  // }
+  else
+  {
+    while (1)
+    {
+      sleep(1);
+      printf("id: %d, kill: %d\n", pid, kill(pid, 0));
+    }
+  }
 }
